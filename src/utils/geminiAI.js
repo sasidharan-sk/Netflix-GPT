ba8cd5cefa8e9f8d76d1f6b7fb34c10769417e71
sasidharan-sk <sasidharanhc450@gmail.com>
2024-04-01 19:28:43 +0530

    Revert "all netflix project files"
    This reverts commit f3d21e44b4e2dd479040d993e9b5e4a5b9385e42.

--- src/utils/geminiAI.js
+++
@@ -1,6 +0,0 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
-import { GEMINI_API_KEY } from "./constants";
-
-const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
-
-export default genAI;
